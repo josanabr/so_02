33f7f1f038b314d60f67b3f0e5b3c154d3cfdedc
John Sanabria <john.sanabria@correounivalle.edu.co>
2021-12-09 16:11:30 +0000

    Primer commit

---
+++ 05/fork-wait/fork-01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+	pid_t pid; // pid_t: pid type
+	printf("[%d] Primera instruccion\n",getpid());
+        pid = fork();
+	if (pid < 0) {
+		printf("Error en la creacion del proceso\n");
+		return 1;
+	} else if (pid == 0) {
+		printf("[%d] Soy el hijo\n",getpid());
+	} else {
+		printf("[%d] Soy el padre y mi hijo es %d\n",getpid(),pid);
+	}
+	printf("[%d] Segunda instruccion\n",getpid());
+	return 0;
+}
