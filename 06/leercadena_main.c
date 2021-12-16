29ca050e2323448f32ddf3994113963508f6201b
John Sanabria <john.sanabria@correounivalle.edu.co>
2021-12-16 16:39:14 +0000

    shell dummy 0.01

--- 06/leercadena_main.c
+++ 06/leercadena_main.c
@@ -8,22 +8,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include "leercadena.h"
 
 int main(int argc, char *argv[]) {
   char comando[BUFSIZ];
   char **vector;
-  int i;
+  pid_t pid;
 
-  printf("Digite su comando ");
-  leer_de_teclado(BUFSIZ,comando);
-  printf("Comando: ");
-  puts(comando);
-  vector = de_cadena_a_vector(comando);
-  i = 0;
-  while (vector[i]) 
-    printf("%s\n",vector[i++]);
-  execvp(vector[0],vector);
+  while (1) {
+    printf("$ ");
+    leer_de_teclado(BUFSIZ,comando);
+    pid = fork();
+    if (pid == 0) {
+      vector = de_cadena_a_vector(comando);
+      execvp(vector[0],vector);
+    } else if (pid > 0) {
+      wait(NULL);
+    } else {
+      printf("Error en la ejecucion del 'fork'\n");
+      return 1;
+    }
+  }
 
   return 0;
 }
